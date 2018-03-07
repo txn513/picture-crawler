e0c37d84b8ff56a8fbad6719476a8a5261466f82
txn513 <xtian513@gmail.com>
2018-03-07 21:39:28 +0800

    update index.js

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ const analyze = require('./analyze');
 const fs = require('fs');
 
 function start(){
-	request(config.url, function(err, res, body){
-		console.log('error:', error); // Print the error if one occurred
-  		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  		console.log('body:', body); // Print the HTML for the Google homepage.
+	request(config.url, function(error, response, body){
+		// console.log('error:', error); // Print the error if one occurred
+  // 		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // 		console.log('body:', body); // Print the HTML for the Google homepage.
 
   		if (!error && response.statusCode == 200) {
-			analyze.findImg(body);       
+			analyze.findImg(body, download);       
 		}
   		
 	});
 }
 
 function download(imgUrl, i){
-	let ext = imgUrl.split('.').pop();
-	request(imgUrl).pipe(fs.createWriteStream(path.join(config.imgDir, i+'.'+ext)));
+	
+	console.log(imgUrl != '');
+	if(imgUrl != ''){
+		let ext = imgUrl.split('.').pop();
+		request(imgUrl).pipe(fs.createWriteStream(path.join(config.imgDir, i+'.'+ext)));
+	}
+	
 }
 
 start();
